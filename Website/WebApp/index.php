d84b35d30cda1720d2448359252c5aed81792adb
bence-d <64145054+bence-d@users.noreply.github.com>
2021-05-25 12:02:58 +0200

    basic implementation of file uploading
    Added a panel to upload a file

--- Website/WebApp/index.php
+++ Website/WebApp/index.php
@@ -1,3 +1,27 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <h1 id="page_title">Image Tagging System</h1>
+
+        <div id="fileUploadHolder">
+            <form action="." method="post" enctype="multipart/form-data">
+            <h3 style="text-align:center;">submit an image</h3>
+            <label for="imageName">Name:</label><br>
+            <input type="text" name="imageName"><br>
+            <label for="userfile">Choose a file to upload</label><br>
+            <input type="file" name="userfile" id="userfile"><br>
+
+            <input type="submit" name="submit" value="Upload">
+            </form>
+        </div>
+    </body>
+</html>
+
 <?php
-  echo "Welcome to our WebApplication"
+    if (isset($_POST['submit'])) {
+        echo "Name: " . $_POST['imageName'] . "<br>"; 
+        echo "Dateiname: " . $_FILES['userfile']['name'];
+    }
 ?>
