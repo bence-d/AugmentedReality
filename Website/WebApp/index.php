7b5ba37dcb5875d1956721385632478a7316701e
bence-d <64145054+bence-d@users.noreply.github.com>
2021-05-26 23:18:43 +0200

    Design & Concept Update
    - Turned the current basic upload scratch into a scratch of a fancy menu of three items: upload, select or delete.

--- Website/WebApp/index.php
+++ Website/WebApp/index.php
@@ -1,27 +1,22 @@
 <html>
-    <head>
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <h1 id="page_title">Image Tagging System</h1>
-
-        <div id="fileUploadHolder">
-            <form action="." method="post" enctype="multipart/form-data">
-            <h3 style="text-align:center;">submit an image</h3>
-            <label for="imageName">Name:</label><br>
-            <input type="text" name="imageName"><br>
-            <label for="userfile">Choose a file to upload</label><br>
-            <input type="file" name="userfile" id="userfile"><br>
-
-            <input type="submit" name="submit" value="Upload">
-            </form>
-        </div>
-    </body>
+  <head>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Cantarell:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles.css">
+    <title>Augmented Reality</title>
+    <style></style>
+  </head>
+  <body>
+    <div id="buttonGroup">
+      <div id="button1" class="buttonHolder">
+        <p class="buttonLabel">+</p>
+      </div>
+      <div id="button2" class="buttonHolder">
+        <p style="padding-top: 3vh;" class="buttonLabel">☰</p>
+      </div>
+      <div id="button3" class="buttonHolder">
+        <p style="margin-top: -1vh;" class="buttonLabel">-</p>
+      </div>
+    </div>
+  </body>
 </html>
-
-<?php
-    if (isset($_POST['submit'])) {
-        echo "Name: " . $_POST['imageName'] . "<br>"; 
-        echo "Dateiname: " . $_FILES['userfile']['name'];
-    }
-?>
