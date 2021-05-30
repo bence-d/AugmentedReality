6dbcdb8d886e6c30c8b70df3908fd2472463a2bc
bence-d <64145054+bence-d@users.noreply.github.com>
2021-05-30 22:50:22 +0200

    Temporary File
    A temporary file for sharing the implementation of auto-image adjustment.

---
+++ Website/WebApp/imageAdjustment/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Auto Image Adjusting</title>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+
+    <body>
+        <h1 id="title">Auto image adjusting</h1>
+        <div id="imageHolder">
+            <img id="image" src="./image.png" alt="random image">
+        </div>
+    </body>
+</html>
