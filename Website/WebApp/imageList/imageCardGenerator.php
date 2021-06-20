5bf29d3fc2db63db9ad61a38075485f5e0a6d318
bence-d <g.drigan@students.htl-leonding.ac.at>
2021-06-20 09:06:35 +0200

    URL Fix
    Image Cards couldn't load their icons due to an incorrect path.

--- Website/WebApp/imageList/imageCardGenerator.php
+++ Website/WebApp/imageList/imageCardGenerator.php
@@ -7,9 +7,9 @@
                 <img class=\"imageHolder_image\" src=\"../Images/" . $imageLocation . "\" alt=\"reference image from DB\">
             </div>
             <div class=\"imageHolder_buttonBar\">
-                <button class=\"imageHolder_button\"><img class=\"buttonIcon\" src=\"/Icons/eye.png\" onclick=\"window.location.replace('/WebApp/imageViewer/?id=" . $id . "')\"></button>
-                <button class=\"imageHolder_button\"><img class=\"buttonIcon\" src=\"/Icons/pen.png\" onclick=\"window.location.replace('/WebApp/imageTaggingNew/?id=" . $id ."')\"></button>
-                <button class=\"imageHolder_button\"><img class=\"buttonIcon\" src=\"/Icons/trash.png\" onclick=\"setDeleteId($id)\"></button>
+                <button class=\"imageHolder_button\"><img class=\"buttonIcon\" src=\"../Icons/eye.png\" onclick=\"window.location.replace('/WebApp/imageViewer/?id=" . $id . "')\"></button>
+                <button class=\"imageHolder_button\"><img class=\"buttonIcon\" src=\"../Icons/pen.png\" onclick=\"window.location.replace('/WebApp/imageTaggingNew/?id=" . $id ."')\"></button>
+                <button class=\"imageHolder_button\"><img class=\"buttonIcon\" src=\"../Icons/trash.png\" onclick=\"setDeleteId($id)\"></button>
             </div>
         </div>
         ";
